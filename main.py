3b61042c8b06ffa541b13054d7da405395eaceb3
Dzmitry Kashchyts <d.kashchyts@gmail.com>
2023-02-19 22:53:40 +0300

    error correction

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 my_name = input("Введи свое имя:\n")
-my_num_grup = int(input("Какой номер твоей группы?\n"))
-print(f"""Привет. Меня зовут {my_name}, я учусь курсу 'Python' в IT-школе 'TeachMeSkills', в группе PY{my_num_grup}.""")
-
+my_num_group = int(input("Какой номер твоей группы?\n"))
+print(f"""Привет. Меня зовут {my_name}, я учусь курсу 'Python' в IT-школе 
+                'TeachMeSkills', в группе PY{my_num_group}.""")
